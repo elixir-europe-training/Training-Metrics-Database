330db01587cb731a3c4dbc142bf3a61c97805c7c
Renato Alves <alves.rjc@gmail.com>
2023-07-19 18:16:38 +0200

    Update settings to prepare for production

--- tango/settings.py
+++ tango/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,14 +19,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^cw%un^vw710_dtxsw9gmk6_0-gax^-&$=acb(0aeprmy_3kkr"
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+if os.environ.get("DJANGO_PRODUCTION", None) is not None:
+    DEBUG = bool(int(os.environ.get("DJANGO_PRODUCTION", 0)))
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    "django-insecure-^cw%un^vw710_dtxsw9gmk6_0-gax^-&$=acb(0aeprmy_3kkr",
+)
+
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+]
 
 # Application definition
 
@@ -78,12 +86,25 @@ WSGI_APPLICATION = "tango.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ["DJANGO_POSTGRESQL_DBNAME"],
+            "USER": os.environ["DJANGO_POSTGRESQL_USER"],
+            "PASSWORD": os.environ["DJANGO_POSTGRESQL_PASSWORD"],
+            "HOST": os.environ["DJANGO_POSTGRESQL_HOST"],
+            "PORT": os.environ["DJANGO_POSTGRESQL_PORT"],
+        }
+    }
+
 
 # User Model
 AUTH_USER_MODEL = "metrics.User"
