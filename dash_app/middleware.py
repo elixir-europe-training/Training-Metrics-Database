a07d74ca2ce749b03938dfadb3ee8a9dd8109075
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2023-07-20 23:31:04 +0200

    Render event metrics in impact using generic mapping

--- dash_app/middleware.py
+++ dash_app/middleware.py
@@ -1,15 +1,49 @@
+import pandas as pd
+import dash_bootstrap_components as dbc
 
+from datetime import datetime
 
+data = pd.read_csv('https://raw.githubusercontent.com/elixir-europe-training/ELIXIR-TrP-Training-Metrics-Database-Tango/main/raw-tmd-data/all_events_expanded.csv')
+
+# convert dates to datetime
+data['Start date'] = pd.to_datetime(data['Start date'], format='%d.%m.%Y')
+data['End date'] = pd.to_datetime(data['End date'], format='%d.%m.%Y')
 
 class Group():
     def __init__(self, fields):
         self.fields = fields
+        self.field_mapping = {
+            '#': "id",
+            'Event code': "event_code",
+            'Title': "title",
+            'ELIXIR Node': "elixir_node",
+            'Start date': "start_date",
+            'End date': "end_date",
+            'Duration': "duration",
+            'Event type': "event_type",
+            'Funding': "funding",
+            'Organising Institution/s': "organizing_inititutions",
+            'Location (city, country)': "location",
+            'EXCELERATE subtask': "excelerate_subtask",
+            'Target audience': "target_audience",
+            'Additional ELIXIR Platforms involved': "additional_platforms",
+            'ELIXIR Communities involved': "A",
+            'Number of participants': "B",
+            'Number of trainers/ facilitators': "C",
+            'Url to event page/ agenda': "D",
+            'Main organiser': "E",
+        }
 
     def get_fields(self):
         return list(self.fields.keys())
     
     def get_filter_fields(self):
-        return self.get_fields()
+        return [
+            "event_type",
+            "funding",
+            "target_audience",
+
+        ]
     
     def get_field_placeholder(self, field_id):
         return field_id
@@ -20,8 +54,26 @@ class Group():
     def get_field_title(self,field_id):
         return field_id
     
-    def get_values(self, **params):
-        return []
+    def get_values(self, event_type=None, funding=None, target_audience=None, date_from=None, date_to=None, node_only=False):
+        data2 = data.copy()
+        if event_type is not None:
+            data2 = data2[data2['Event type'] == event_type]
+        if funding is not None:
+            data2 = data2[data2['Funding'] == funding]
+        if target_audience is not None:
+            data2 = data2[data2['Target audience'] == target_audience]
+        if date_from is not None and date_to is not None:
+            data2 = data2[(data2['Year'] >= date_from) & (data2['Year'] <= date_to)]
+        if node_only:
+            data2 = data2[data2['Main organiser'] == 'ELIXIR-SE'] # CHANGE THIS to USER's node
+        records = [
+            {
+                self.field_mapping[key]: value
+                for key, value in row.items()
+            }
+            for row in data2.to_dict(orient='records')
+        ]
+        return records
 
 
 class Metrics():
@@ -29,16 +81,17 @@ class Metrics():
         self.groups = groups
 
     def get_group(self, name):
-        return self.groups[name]
+        return self.groups.get(name, None)
 
 
 current_metric = Metrics(
     {
-        "impact": Group(
+        "events": Group(
             {
-                "event_type": ["A", "B", "C"], 
-                "funding": ["A", "B", "C"], 
-                "target_audience": ["A", "B", "C"]
+                "event_type": sorted(list(set(data['Event type']))), 
+                "funding": sorted([str(x) for x in set(data['Funding']) if str(x) != 'nan']), 
+                "target_audience": sorted([str(x) for x in set(data['Target audience']) if str(x) != 'nan']),
+                "location": sorted(list(set(data['Location (city, country)'])))
             }
         )
     }
--- dash_app/views/impact.py
+++ dash_app/views/impact.py
@@ -10,50 +10,74 @@ from dash.dependencies import Output, Input
 from datetime import datetime
 from functools import lru_cache
 from .common import get_tabs
+from itertools import groupby
 
 
 app = DjangoDash("ImpactReport")
 
 
+def calculate_metrics(data, column):
+    column_values = [d[column] for d in data]
+    count = {}
+    for v in column_values:
+        count[v] = count.get(v, 0) + 1
+    return count
+
+
+def generate_figure(metrics, title, xaxis, yaxis):
+    return {
+        'data': [
+            go.Bar(
+                y=[value for value in metrics.values()],
+                x=[key for key in metrics.keys()],
+            )
+        ],
+        'layout': go.Layout(
+            title=title,
+            xaxis={'title': xaxis, 'tickfont': {'size': 10}},
+            yaxis={'title': yaxis},
+        )
+    }
+
+
+def generate_tables_and_figures(group, values):
+    for field_id in group.get_fields():
+        metrics = calculate_metrics(values, field_id)
+        yield generate_figure(
+            metrics,
+            group.get_field_title(field_id),
+            'No. of Events',
+            group.get_field_title(field_id)
+        )
+        yield [
+            {"name": name, "value": value}
+            for name, value in metrics.items()
+        ]
+
+
 def get_callback(group):
     def update_graph_and_table(*filter_values):
         filters = list(zip([*group.get_filter_fields(), "date_from", "date_to", "node_only"], filter_values))
-        metrics = group.get_values(**{
-            "name": value
+        values = group.get_values(**{
+            name: value
             for name, value in filters
         })
         
-        def generate_table_and_figure(data, column, title):
-            figure = {
-                'data': [
-                    go.Bar(
-                        x=[],
-                        y=[],
-                    )
-                ],
-                'layout': go.Layout(
-                    title=title,
-                    xaxis={'title': 'No. of Events', 'tickfont': {'size': 10}},
-                    yaxis={'title': column},
-                )
-            }
-            return figure
-
-        return [
-            generate_table_and_figure(metrics, field_id, group.get_field_title(field_id))
-            for field_id in group.get_fields()
-        ]
+        return list(generate_tables_and_figures(group, values))
     
     return update_graph_and_table
+
+
+def get_outputs(group):
+    for field_id in group.get_fields():
+        yield Output(f'{field_id}-graph', 'figure')
+        yield Output(f'{field_id}-table', 'data')
 
 
 @lru_cache
 def use_callback(app, group):
     app.callback(
-        [
-            Output(f'{field_id}-graph', 'figure')
-            for field_id in group.get_fields()
-        ],
+        list(get_outputs(group)),
         [
             *[
                 Input(field_id, 'value')
@@ -98,6 +122,15 @@ def get_layout(app, group):
             ]),
             *[
                 dbc.Row([
+                    dash_table.DataTable(
+                        id=f'{field_id}-table',
+                        columns=[
+                            {"name": group.get_field_title(field_id), "id": "name"},
+                            {"name": 'Events', "id": "value"}
+                        ],
+                        page_size=10,
+                        style_cell={'textAlign': 'left'},
+                    ),
                     dcc.Graph(id=f'{field_id}-graph')
                 ])
                 for field_id in group.get_fields()
@@ -107,16 +140,21 @@ def get_layout(app, group):
 
 
 def impact_report(request):
-    group = request.metrics.get_group('impact')
-    app.layout = get_layout(app, group)
-    use_callback(app, group)
+    group = request.metrics.get_group('events')
+    dash_config = {}
+    if group:
+        app.layout = get_layout(app, group)
+        use_callback(app, group)
+        dash_config = {
+            "dash_name": "ImpactReport"
+        }
 
     return render(
         request,
         'dash_app/template.html',
         context={
             **get_tabs(request),
-            "dash_name": "ImpactReport"
+            **dash_config
         }
     )
 
