69dff75a0f192198385ae5e8a3167cf463fce2a6
Maria Doyle <maria.doyle@ul.ie>
2023-07-20 10:24:36 +0100

    Add table to events

--- dash_app/views.py
+++ dash_app/views.py
@@ -3,28 +3,29 @@ import plotly.graph_objs as go
 import pandas as pd
 import dash_bootstrap_components as dbc
 
-
 from django.shortcuts import render
 from django_plotly_dash import DjangoDash
-from dash import dcc
-from dash import html
+from dash import dcc, html, dash_table
+from dash.dependencies import Output, Input
 
-from datetime import date, datetime
+from datetime import datetime
 
 app = DjangoDash("SimpleExample")
-
 
 # read data from csv for now. To be replaced with db queries
 data = pd.read_csv('https://raw.githubusercontent.com/elixir-europe-training/ELIXIR-TrP-Training-Metrics-Database-Tango/main/raw-tmd-data/all_events_expanded.csv')
 data['Year'] = pd.to_numeric(data['Start date'].str.split('.').str[-1])
 
+# Initialize the table with all event types and zero count
+data['Number of events'] = 0
+table_data_initial = data[['Event type', 'Number of events']].drop_duplicates().to_dict('records')
+
 # extract values from all columns for plotting
 event_type = sorted(list(set(data['Event type'])))
-funding    = [x for x in list(set(data['Funding'])) if str(x) != 'nan']
-location   = sorted(list(set(data['Location (city, country)'])))
-#put rest of filtering variables here
+funding = [x for x in list(set(data['Funding'])) if str(x) != 'nan']
+location = sorted(list(set(data['Location (city, country)'])))
 
-# layout for the input parameters for filtering (NEEDS UPDATING)
+# layout for the input parameters for filtering
 app.layout = html.Div([
     dbc.Row([
         dbc.Col([
@@ -32,81 +33,92 @@ app.layout = html.Div([
                 id='event-type',
                 options=[{'label': i, 'value': i} for i in event_type],
                 value='Event_type'
-        )]),
+            )
+        ]),
         dbc.Col([
             dcc.Dropdown(
                 id='funding-type',
                 options=[{'label': i, 'value': i} for i in funding],
                 value='funding'
-        )])
+            )
+        ])
     ]),
     dbc.Row([
         dbc.Col([
             dcc.DatePickerRange(
-            id='date-picker-range',
-            initial_visible_month=datetime.now(),
+                id='date-picker-range',
+                initial_visible_month=datetime.now(),
             ),
-        ], width={'size':2,'offset':0,'order':'1'}),
+        ], width={'size': 2, 'offset': 0, 'order': '1'}),
         dbc.Col([
             dcc.Dropdown(
                 id='location-picker',
                 options=[{'label': i, 'value': i} for i in location],
                 value='location'
             ),
-        ], width={'size':2,'offset':0,'order':'2'})
+        ], width={'size': 2, 'offset': 0, 'order': '2'})
     ]),
+    dash_table.DataTable(
+        id='event-table',
+        columns=[{"name": 'Event type', "id": 'Event type'}, {"name": 'Events', "id": 'Events'}],
+        data=table_data_initial,
+        page_size=10,
+        style_cell={'textAlign': 'left'},
+    ),
     dbc.Row([
         dcc.Graph(
-        id='event-type-graph',
-        figure={
-            'data': [
-                go.Histogram(
-                    x=data['Event type'],
-                    name='Events'
+            id='event-type-graph',
+            figure={
+                'data': [
+                    go.Histogram(
+                        x=data['Event type'],
+                        name='Events'
+                    )
+                ],
+                'layout': go.Layout(
+                    title='Event type',
+                    xaxis={'title': 'Event type'},
+                    yaxis={'title': 'No. of events'},
                 )
-            ],
-            'layout': go.Layout(
-                title='Event Type Bar Plot',
-                xaxis={'title': 'Event Types'},
-                yaxis={'title': 'Number of Events'},
-            )
-        }
-    )
+            }
+        )
     ])
-    
 ])
 
-
-# updating the plot
+# updating the plot and table
 @app.callback(
-    dash.dependencies.Output('event-type-graph', 'figure'),
-    [dash.dependencies.Input('location-picker', 'value'),
-     dash.dependencies.Input('funding-type', 'value')])
-def update_graph(location, funding):
-    data2 = data['Event type']
+    [Output('event-table', 'data'),
+     Output('event-type-graph', 'figure')],
+    [Input('location-picker', 'value'),
+     Input('funding-type', 'value')]
+)
+def update_graph_and_table(location, funding):
+    data2 = data.copy()  # copy the original data
     if location != 'location':
-        data2 = data[data['Location (city, country)'] == location]['Event type']
-    
-    if funding != 'funding':
-        data2 = data[data['Funding'] == funding]['Event type']
-    
-  #  data2 = data[(data['Location (city, country)'] == location) & (data['Funding'] == funding)]
+        data2 = data2[data2['Location (city, country)'] == location]
 
-    return {
+    if funding != 'funding':
+        data2 = data2[data2['Funding'] == funding]
+
+    figure = {
         'data': [
-                go.Histogram(
-                    x=data2,
-                    name='Events'
-                )
-            ],
-            'layout': go.Layout(
-                title='Event Type Bar Plot',
-                xaxis={'title': 'Event Types'},
-                yaxis={'title': 'Number of Events'},
+            go.Histogram(
+                x=data2['Event type'],
+                name='Events'
             )
+        ],
+        'layout': go.Layout(
+            title='Event type',
+            xaxis={'title': 'Event type'},
+            yaxis={'title': 'No. of events'},
+        )
     }
 
+    # Group by 'Event type' and count the occurrences
+    table_data = data2.groupby('Event type').size().reset_index(name='Events')
+    table_data = table_data[['Event type', 'Events']].to_dict('records')
 
+    return table_data, figure
 
 def event_report(request):
     return render(
