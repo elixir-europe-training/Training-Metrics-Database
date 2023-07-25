37026da28c7a7581c8efab6747d9f094de9fce2a
Mihail Anton <mihail.anton@chalmers.se>
2023-07-25 21:15:24 +0200

    feat: detail node contribution on world map

--- dash_app/views/worldmap.py
+++ dash_app/views/worldmap.py
@@ -5,17 +5,22 @@ import plotly.express as px
 from dash import dcc
 from dash import html
 import pandas as pd
-from django.db.models import Count
-from metrics.models import Event
+from django.db.models import Count, F
+from metrics.models import Event, Node
 
 def get_event_data():
     # Query database to get counts of events by country
-    event_counts = Event.objects.values('location_country').annotate(count=Count('code', distinct=True))
-
+    event_counts = Event.objects.filter(node_main=F('node')).values('location_country', 'node_main__name').annotate(count=Count('code', distinct=True))
     # Convert the QuerySet to a DataFrame
     df = pd.DataFrame(list(event_counts))
     total_events = df['count'].sum()
-    return df, total_events
+    temp = df.groupby('location_country').agg(lambda x: pd.Series.tolist(x))
+    temp['aggregated']=temp[['node_main__name', 'count']].agg(lambda x: "; ".join(x[0][i] + ": " + str(x[1][i]) for i in range(len(x[0]))), axis=1)
+    temp = temp.drop(columns=['node_main__name', 'count'])
+    df = df.drop(columns=['node_main__name'])
+    df = df.groupby('location_country').sum().reset_index()
+    final = pd.merge(df, temp, on='location_country')
+    return final, total_events
 
 def world_map(request):
     app = DjangoDash("WorldMap")
@@ -30,9 +35,10 @@ def world_map(request):
                         locationmode='country names',
                         color_continuous_scale=px.colors.sequential.Viridis,
                         title=f'Number of events per country (Total: {format(total_events, ",")})',
+                        custom_data=['aggregated'],
                         height=600)
 
-    fig.update_traces(hovertemplate='%{location}<br>%{z}<extra></extra>')
+    fig.update_traces(hovertemplate='%{location} - %{z} events<br>%{customdata}<extra></extra>')
 
     fig.update_layout(
         title={
@@ -43,11 +49,12 @@ def world_map(request):
         },
         geo=dict(
             showframe=False,
-            showcoastlines=False
+            showcoastlines=False,
+            showcountries=True,
+            countrywidth=0.2,
+            lataxis_range=[-60, 90]
         )
     )
-
-    fig.update_geos(showcountries=True, countrywidth=0.2, lataxis_range=[-60, 90])
 
     app.layout = html.Div([
         dcc.Graph(id='choropleth', figure=fig)
