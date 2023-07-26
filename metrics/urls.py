ebbb36001e49ced70c12365d2c6656ff6916e9be
jonandernovella <novella.uu@gmail.com>
2023-07-26 12:40:59 +0200

    Remove unused view and url

--- metrics/urls.py
+++ metrics/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='metrics/login.html',
                                      authentication_form=UserLoginForm, next_page='/'), name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
-    path('editevent/', views.EventFormView.as_view(), name='event_detail'),
 ]
