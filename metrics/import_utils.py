7a81fe8f2553645237a90a50d4eb1965a686476f
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2024-05-13 10:05:59 +0200

    refactor: move import code to common import_utils

---
+++ metrics/import_utils.py
@@ -0,0 +1,257 @@
+from datetime import datetime
+from metrics.models import Event, Demographic, Quality, Impact, User, Node
+from django.utils.text import slugify
+
+
+class ImportContext:
+    def __init__(self, user=None, node_main=None):
+        self._user = user
+        self._node_main = node_main
+
+    def event_from_dict(self, data: dict):
+        (created, modified) = timestamps_from_dict(data)
+
+        event = Event.objects.create(
+            user=self.user_from_data(data),
+            created=created,
+            modified=modified,
+            code=slugify(data['code']),
+            title=data['title'],
+            node_main=self.node_from_data(data),
+            date_start=convert_to_date(data['date_start']),
+            date_end=convert_to_date(data['date_end']),
+            duration=float(data['duration']),
+            type=data['type'],
+            funding=csv_to_array(data['funding']),
+            location_city=data['location_city'],
+            location_country=data['location_country'],
+            target_audience=csv_to_array(data['target_audience']),
+            additional_platforms=csv_to_array(data['additional_platforms']),
+            communities=csv_to_array(data['communities']),
+            number_participants=data['number_participants'],
+            number_trainers=data['number_trainers'],
+            url=data['url'],
+            status=data['status'],
+        )
+        # event.organising_institution.set([data['organising_institution']])
+        node_names = data['node'].split(",")
+        stripped_names = [name.strip() for name in node_names]
+        nodes = [
+            Node.objects.get(name=node)
+            for node in stripped_names
+        ]
+        event.node.add(*nodes)
+
+        return event
+
+    def demographic_from_dict(self, data: dict):
+        (created, modified) = timestamps_from_dict(data)
+        demographic = Demographic.objects.create(
+            user=self.user_from_data(data),
+            created=created,
+            modified=modified,
+            event=Event.objects.get(code=int(data['event'])),
+            heard_from=csv_to_array(data['heard_from']),
+            employment_sector=data['employment_sector'],
+            employment_country=data['employment_country'],
+            gender=data['gender'],
+            career_stage=data['career_stage'],
+        )
+        return demographic
+
+    def quality_from_dict(self, data: dict):
+        (created, modified) = timestamps_from_dict(data)
+        quality = Quality.objects.create(
+            user=self.user_from_data(data),
+            created=created,
+            modified=modified,
+            event=Event.objects.get(code=data['event']),
+            used_resources_before=data['used_resources_before'],
+            used_resources_future=data['used_resources_future'],
+            recommend_course=data['recommend_course'],
+            course_rating=data['course_rating'],
+            balance=data['balance'],
+            email_contact=data['email_contact'],
+        )
+        return quality
+
+    def impact_from_dict(self, data: dict):
+        (created, modified) = timestamps_from_dict(data)
+        impact = Impact.objects.create(
+            user=self.user_from_data(data),
+            created=created,
+            modified=modified,
+            event=Event.objects.get(code=data['event']),
+            when_attend_training=data['when_attend_training'],
+            main_attend_reason=data['main_attend_reason'],
+            how_often_use_before=data['how_often_use_before'],
+            how_often_use_after=data['how_often_use_after'],
+            able_to_explain=data['able_to_explain'],
+            able_use_now=data['able_use_now'],
+            help_work=csv_to_array(data['help_work']),
+            attending_led_to=csv_to_array(data['attending_led_to']),
+            people_share_knowledge=data['people_share_knowledge'],
+            recommend_others=data['recommend_others'],
+        )
+        return impact
+
+    def user_from_data(self, data: dict):
+        return (
+            User.objects.get(username=data['user'])
+            if self._user is None
+            else self._user
+        )
+
+    def node_from_data(self, data: dict):
+        node_main = data['node_main'] if data['node_main'] else ''
+        return (
+            Node.objects.get(
+                name=node_main
+            ) if node_main and self._node_main is None
+            else self._node_main
+        )
+
+    @staticmethod
+    def from_request(request):
+        return ImportContext(user=request.user)
+
+
+def csv_to_array(csv_string):
+    return [x for x in csv_string.split(",")] if csv_string else []
+
+
+def convert_to_timestamp(date_string):
+    return datetime.strptime(date_string, '%Y-%m-%d %H:%M:%S').timestamp()
+
+
+def convert_to_date(date_string):
+    return datetime.strptime(date_string, '%Y-%m-%d').date()
+
+
+def timestamps_from_dict(data: dict):
+    created = (
+        convert_to_timestamp(data['created'])
+        if data['created']
+        else ''
+    )
+    modified = (
+        convert_to_timestamp(data['modified'])
+        if data['modified']
+        else ''
+    )
+    return (created, modified)
+
+
+def legacy_to_current_event_dict(data: dict) -> dict:
+    mapping = {
+        "Title": "title",
+        "ELIXIR Node": "node",
+        "Start Date": "date_start",
+        "End Date": "date_end",
+        "Event type": "type",
+        "Funding": "funding",
+        "Organising Institution/s": "organising_institution",
+        "Location (city, country)": parse_location,
+        "EXCELERATE WP": None,
+        "Target audience": "target_audience",
+        "Additional ELIXIR Platforms involved": "additional_platforms",
+        "ELIXIR Communities involved": "communities",
+        "No. of participants": "number_participants",
+        "No. of trainers/ facilitators": "number_trainers",
+        "Url to event page/ agenda": "url",
+        "node_main": "node_main",
+        "duration": "duration",
+        "status": "status",
+    }
+    return parse_with_mapping(data, mapping)
+
+
+def legacy_to_current_quality_or_demographic_dict(data: dict) -> dict:
+    mapping = {
+        "event_code": "event",
+        "Where did you see the course advertised?": None,
+        "What is your career stage?": "career_stage",
+        "What is your employment sector?": "employment_country",
+        "What is your country of employment?": "employment_country",
+        "What is your gender?": "gender",
+        "Have you used the tool(s)/resource(s) covered in the course before?": "used_resources_before",
+        "Will you use the tool(s)/resource(s) covered in the course again?": "used_resources_future",
+        "Would you recommend the course?": "recommend_course",
+        "Please tell us your overall rating for the entire course": "course_rating",
+        "May we contact you by email in the future for more feedback?": "email_contact",
+        "What part of the training did you enjoy the most?": None,
+        "What part of the training did you enjoy the least?": None,
+        "The balance of theoretical and practical content was": "balance",
+        "What other topics would you like to see covered in the future?": None,
+        "Any other comments?": None,
+    }
+    return parse_with_mapping(data, mapping)
+
+
+def legacy_to_current_impact_dict(data: dict) -> dict:
+    mapping = {
+        "event_code": "event",
+        "Which training event did you take part in?": None,
+        "How long ago did you attend the training?": "when_attend_training",
+        "What was your main reason for attending the training?": "main_attend_reason",
+        "What was your main reason for attending the training? (Other)": None,
+        "How often did you use the tool(s)/ resource(s), covered in the training, BEFORE attending the training?": "how_often_use_before",
+        "How often do you use the tool(s)/ resource(s), covered in the training, AFTER having attended the training?": "how_often_use_after",
+        "Do you feel that you are able to explain to others what you learnt in the training?": "able_to_explain",
+        "Do you feel that you are able to explain to others what you learnt in the training? (Other)": None,
+        "Are you now able to use the tool(s)/ resource(s) covered in the training:": "able_use_now",
+        "Are you now able to use the tool(s)/ resource(s) covered in the training: (Other)": None,
+        "How did the training event help with your work? [select all that apply]": "help_work",
+        "How did the training event help with your work? (Other)": None,
+        "Attending the training event led to/ facilitated: [select all that apply]": "attending_led_to",
+        "Attending the training event led to/ facilitated: (Other)": None,
+        "Please elaborate on any impact": None,
+        "How many people have you shared the skills and/or knowledge that you learned during the training, with?": "people_share_knowledge",
+        "Would you recommend the training to others?": "recommend_others",
+        "Any other comments?": None,
+    }
+    return parse_with_mapping(data, mapping)
+
+
+def parse_with_mapping(data: dict, mapping: dict) -> dict:
+    transforms = [
+        (
+            parse_entry(source_id, parse_value(target))
+            if type(target) == str
+            else parse_entry(source_id, target)
+        )
+        for source_id, target in mapping.items()
+        if target is not None
+    ]
+
+    result = dict()
+    for transform in transforms:
+        result.update(transform(data))
+    return result
+
+
+def parse_value(target_id, transform=None) -> dict:
+    def _parse_value(value):
+        return {
+            target_id: (
+                value
+                if transform is None
+                else transform(value)
+            )
+        }
+    return _parse_value
+
+
+def parse_entry(source_id, parse_value):
+    def _parse_entry(data):
+        return parse_value(data[source_id])
+    return _parse_entry
+
+
+def parse_location(data: dict) -> dict:
+    location = data["location"]
+    [city, country] = [value.strip() for value in location.split(",")]
+    return {
+        "location_city": city,
+        "location_country": country
+    }
--- metrics/management/commands/load_data.py
+++ metrics/management/commands/load_data.py
@@ -1,9 +1,7 @@
 import csv
-from datetime import datetime
-from django.utils.text import slugify
 from metrics.models import Event, Demographic, Quality, Impact, Node, OrganisingInstitution, User
+from metrics import import_utils
 from django.core.management.base import BaseCommand
-from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
@@ -20,18 +18,7 @@ def get_data_sources(targetdir="example-data"):
 
 
 DATA_SOURCES = get_data_sources()
-
-
-def convert_to_timestamp(date_string):
-    return datetime.strptime(date_string, '%Y-%m-%d %H:%M:%S').timestamp()
-
-
-def convert_to_date(date_string):
-    return datetime.strptime(date_string, '%Y-%m-%d').date()
-
-
-def csv_to_array(csv_string):
-    return [x for x in csv_string.split(",")] if csv_string else []
+import_context = import_utils.ImportContext()
 
 
 def are_headers_in_model(csv_file_path, model):
@@ -51,116 +38,28 @@ def load_events():
     with open(DATA_SOURCES[Event], newline='') as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',')
         for row in reader:
-            created = convert_to_timestamp(
-                row['created']) if row['created'] else ''
-            modified = convert_to_timestamp(
-                row['modified']) if row['modified'] else ''
-
-            node_main = row['node_main'] if row['node_main'] else ''
-
-            event = Event.objects.create(
-                user=User.objects.get(username=row['user']),
-                created=created,
-                modified=modified,
-                code=slugify(row['code']),
-                title=row['title'],
-                node_main=Node.objects.get(
-                    name=node_main) if node_main else None,
-                date_start=convert_to_date(row['date_start']),
-                date_end=convert_to_date(row['date_end']),
-                duration=float(row['duration']),
-                type=row['type'],
-                funding=csv_to_array(row['funding']),
-                location_city=row['location_city'],
-                location_country=row['location_country'],
-                target_audience=csv_to_array(row['target_audience']),
-                additional_platforms=csv_to_array(row['additional_platforms']),
-                communities=csv_to_array(row['communities']),
-                number_participants=row['number_participants'],
-                number_trainers=row['number_trainers'],
-                url=row['url'],
-                status=row['status'],
-            )
-            # event.organising_institution.set([row['organising_institution']])
-            node_names = row['node'].split(",")
-            stripped_names = [name.strip() for name in node_names]
-            nodes = [Node.objects.get(name=node)
-                     for node in stripped_names]
-            event.node.add(*nodes)
-
-            # Adding ManyToMany relationship with OrganisingInstitution
-            # institutions = [OrganisingInstitution.objects.get(
-            #    name=inst) for inst in row['organising_institution'].split(",")]
-            # event.organising_institution.add(*institutions)
+            import_context.event_from_dict(row)
 
 
 def load_demographics():
     with open(DATA_SOURCES[Demographic], newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            created = convert_to_timestamp(
-                row['created']) if row['created'] else ''
-            modified = convert_to_timestamp(
-                row['modified']) if row['modified'] else ''
-            Demographic.objects.create(
-                user=User.objects.get(username=row['user']),
-                created=created,
-                modified=modified,
-                event=Event.objects.get(code=int(row['event'])),
-                heard_from=csv_to_array(row['heard_from']),
-                employment_sector=row['employment_sector'],
-                employment_country=row['employment_country'],
-                gender=row['gender'],
-                career_stage=row['career_stage'],
-            )
+            import_context.demographic_from_dict(row)
 
 
 def load_qualities():
     with open(DATA_SOURCES[Quality], newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            created = convert_to_timestamp(
-                row['created']) if row['created'] else ''
-            modified = convert_to_timestamp(
-                row['modified']) if row['modified'] else ''
-            Quality.objects.create(
-                user=User.objects.get(username=row['user']),
-                created=created,
-                modified=modified,
-                event=Event.objects.get(code=row['event']),
-                used_resources_before=row['used_resources_before'],
-                used_resources_future=row['used_resources_future'],
-                recommend_course=row['recommend_course'],
-                course_rating=row['course_rating'],
-                balance=row['balance'],
-                email_contact=row['email_contact'],
-            )
+            import_context.quality_from_dict(row)
 
 
 def load_impacts():
     with open(DATA_SOURCES[Impact], newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            created = convert_to_timestamp(
-                row['created']) if row['created'] else ''
-            modified = convert_to_timestamp(
-                row['modified']) if row['modified'] else ''
-            Impact.objects.create(
-                user=User.objects.get(username=row['user']),
-                created=created,
-                modified=modified,
-                event=Event.objects.get(code=row['event']),
-                when_attend_training=row['when_attend_training'],
-                main_attend_reason=row['main_attend_reason'],
-                how_often_use_before=row['how_often_use_before'],
-                how_often_use_after=row['how_often_use_after'],
-                able_to_explain=row['able_to_explain'],
-                able_use_now=row['able_use_now'],
-                help_work=csv_to_array(row['help_work']),
-                attending_led_to=csv_to_array(row['attending_led_to']),
-                people_share_knowledge=row['people_share_knowledge'],
-                recommend_others=row['recommend_others'],
-            )
+            import_context.impact_from_dict(row)
 
 
 def load_user():
