d9e0e1b062b2123d46d472f2028dfbd996c30e24
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2024-11-05 18:28:14 +0100

    wip: improve admin experience for questions and sets

--- app/metrics/admin.py
+++ app/metrics/admin.py
@@ -13,9 +13,18 @@ from metrics.models import (
 )
 
 
+EDIT_TRACKING_FIELDS = [
+    "user"
+]
+
+
 @admin.register(Event)
 class EventAdmin(ModelAdmin):
-    pass
+    exclude = EDIT_TRACKING_FIELDS
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        return super().save_model(request, obj, form, change)
 
 
 class ResponseAdmin(admin.TabularInline):
@@ -24,23 +33,52 @@ class ResponseAdmin(admin.TabularInline):
 
 @admin.register(ResponseSet)
 class ResponseSetAdmin(ModelAdmin):
+    exclude = EDIT_TRACKING_FIELDS
     inlines = [
         ResponseAdmin,
     ]
+    list_display = (
+        "event",
+        "user",
+    )
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        return super().save_model(request, obj, form, change)
 
 
 @admin.register(QuestionSet)
 class QuestionSetAdmin(ModelAdmin):
-    pass
+    exclude = EDIT_TRACKING_FIELDS
+
+    list_display = (
+        "name",
+        "user",
+    )
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        return super().save_model(request, obj, form, change)
 
 
 @admin.register(QuestionSuperSet)
 class QuestionSuperSetAdmin(ModelAdmin):
-    pass
+    exclude = EDIT_TRACKING_FIELDS
+
+    list_display = (
+        "name",
+        "node",
+        "user",
+    )
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        return super().save_model(request, obj, form, change)
 
 
 class AnswerAdmin(admin.TabularInline):
     model = Answer
+    exclude = EDIT_TRACKING_FIELDS
 
     def get_prepopulated_fields(self, request, obj=None):
         return {"slug": ["text"]}
@@ -49,9 +87,20 @@ class AnswerAdmin(admin.TabularInline):
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ["text"]}
+    exclude = EDIT_TRACKING_FIELDS
     list_display = (
         "text",
+        "user",
     )
     inlines = [
         AnswerAdmin,
     ]
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        return super().save_model(request, obj, form, change)
+    
+    def save_formset(self, request, form, formset, change):
+        for instance in formset.save(commit=False):
+            instance.user = request.user
+        return super().save_formset(request, form, formset, change)
