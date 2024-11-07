2f016aaa55769d3c925786ebcd8d8cf790e707b4
ninanorgren <nina.norgren@hotmail.com>
2024-11-07 09:12:55 +0100

    started adding csv template download buttons

--- app/metrics/views/upload.py
+++ app/metrics/views/upload.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from metrics.views.common import get_tabs
 from django import forms
 from django.forms.widgets import FileInput, Select, CheckboxInput
@@ -11,6 +11,8 @@ from django.core.exceptions import ValidationError
 from django.db import transaction
 import datetime
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from metrics.models.questions import QuestionSuperSet
 
 
 UPLOAD_TYPES = {
@@ -82,6 +84,8 @@ def table_output(columns: dict):
 
 @login_required
 def upload_data(request):
+    node = request.user.get_node()
+    question_supersets = QuestionSuperSet.objects.filter(node = node)
     forms = [
         DataUploadForm(
             request.POST if request.method == "POST" else None,
@@ -166,7 +170,46 @@ def upload_data(request):
         'metrics/upload.html',
         context={
             "title": "Upload data",
+            "question_supersets": question_supersets,
             **get_tabs(request),
             "forms": forms,
         }
     )
+
+
+@login_required
+def download_event_template(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="event_template.csv"'
+
+    # Write CSV data to the response
+    writer = csv.writer(response)
+    writer.writerow(['Question Text', 'Description', 'Slug', 'Is Multi-choice'])  # Define headers
+    for upload_type in UPLOAD_TYPES.values():
+        print(upload_type)
+    # Fetch questions from the database and write each to the CSV
+ #   questions = Question.objects.all()
+ #   for question in questions:
+ #       writer.writerow([question.text, question.description, question.slug, question.is_multichoice])
+
+    return response
+
+def download_questionsuperset_template(request, questionsuperset_id):
+    # Get the specific QuestionSuperSet instance
+    questionsuperset = get_object_or_404(QuestionSuperSet, id=questionsuperset_id)
+    
+    # Set up the CSV response
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = f'attachment; filename="{questionsuperset.name}_template.csv"'
+    
+    # Create a CSV writer
+    writer = csv.writer(response)
+    writer.writerow(['Question Text', 'Description', 'Slug', 'Is Multi-choice'])  # Define CSV headers
+    
+    # Write each question in the QuestionSuperSet to the CSV
+    for set in questionsuperset.question_sets.all():
+        for question in set.questions.all():
+            print(question)
+       # writer.writerow([question.text, question.description, question.slug, question.is_multichoice])
+    
+    return response
