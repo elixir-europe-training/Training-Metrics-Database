52bb19519107c739de64194f99bca34273b7fd06
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2024-11-08 12:59:10 +0100

    feat: add initial flexible question model

--- app/metrics/admin.py
+++ app/metrics/admin.py
@@ -2,9 +2,107 @@ from django.conf import settings
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
-from metrics.models import Event
+from metrics.models import (
+    Event,
+    Question,
+    QuestionSet,
+    QuestionSuperSet,
+    Answer,
+    Response,
+    ResponseSet
+)
+
+
+EDIT_TRACKING_FIELDS = [
+    "user"
+]
 
 
 @admin.register(Event)
 class EventAdmin(ModelAdmin):
-    pass
+    exclude = EDIT_TRACKING_FIELDS
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        return super().save_model(request, obj, form, change)
+
+
+class ResponseAdmin(admin.TabularInline):
+    model = Response
+
+
+@admin.register(ResponseSet)
+class ResponseSetAdmin(ModelAdmin):
+    exclude = EDIT_TRACKING_FIELDS
+    inlines = [
+        ResponseAdmin,
+    ]
+    list_display = (
+        "event",
+        "user",
+    )
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        return super().save_model(request, obj, form, change)
+
+
+@admin.register(QuestionSet)
+class QuestionSetAdmin(ModelAdmin):
+    exclude = EDIT_TRACKING_FIELDS
+    prepopulated_fields = {"slug": ["name"]}
+
+    list_display = (
+        "name",
+        "user",
+    )
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        return super().save_model(request, obj, form, change)
+
+
+@admin.register(QuestionSuperSet)
+class QuestionSuperSetAdmin(ModelAdmin):
+    exclude = EDIT_TRACKING_FIELDS
+    prepopulated_fields = {"slug": ["name"]}
+
+    list_display = (
+        "name",
+        "node",
+        "user",
+    )
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        return super().save_model(request, obj, form, change)
+
+
+class AnswerAdmin(admin.TabularInline):
+    model = Answer
+    exclude = EDIT_TRACKING_FIELDS
+
+    def get_prepopulated_fields(self, request, obj=None):
+        return {"slug": ["text"]}
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ["text"]}
+    exclude = EDIT_TRACKING_FIELDS
+    list_display = (
+        "text",
+        "user",
+    )
+    inlines = [
+        AnswerAdmin,
+    ]
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        return super().save_model(request, obj, form, change)
+    
+    def save_formset(self, request, form, formset, change):
+        for instance in formset.save(commit=False):
+            instance.user = request.user
+        return super().save_formset(request, form, formset, change)
---
+++ app/metrics/models/questions.py
@@ -0,0 +1,68 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+from .common import EditTracking, Event, Node
+
+
+class Question(EditTracking):
+    text = models.CharField(max_length=1024)
+    description = models.CharField(max_length=2048, blank=True, null=True)
+    slug = models.SlugField(default="", null=False, unique=True, max_length=1024)
+    is_multichoice = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.text} ({self.slug})"
+
+
+class QuestionSet(EditTracking):
+    name = models.TextField(max_length=1024)
+    slug = models.SlugField(default="", null=False, unique=True, max_length=1024)
+    questions = models.ManyToManyField(Question)
+
+    def __str__(self):
+        return self.name
+
+
+class QuestionSuperSet(EditTracking):
+    name = models.TextField(max_length=1024)
+    slug = models.SlugField(default="", null=False, unique=True, max_length=1024)
+    node = models.ForeignKey(Node, on_delete=models.PROTECT, blank=True, null=True)
+    question_sets = models.ManyToManyField(QuestionSet)
+
+    def __str__(self):
+        node_name = self.node.name if self.node else "Shared"
+        return f"{self.name} ({node_name})"
+
+
+class Answer(EditTracking):
+    text = models.CharField(max_length=1024)
+    slug = models.SlugField(default="", null=False, max_length=1024)
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name="answers"
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["slug", "question"],
+                name="slug is unique per question",
+            )
+        ]
+
+    def __str__(self):
+        return self.text
+
+
+class ResponseSet(EditTracking):
+    event = models.ForeignKey(
+        Event, on_delete=models.PROTECT, related_name="responses"
+    )
+    question_set = models.ForeignKey(
+        QuestionSet, on_delete=models.PROTECT
+    )
+
+
+class Response(models.Model):
+    response_set = models.ForeignKey(
+        ResponseSet, on_delete=models.CASCADE, related_name="entries"
+    )
+    answer = models.ForeignKey(Answer, on_delete=models.PROTECT)
