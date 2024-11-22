af6d3a43e20c08f4084d6080f6525c623a594f2d
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2024-11-22 16:38:19 +0100

    feat: add statisitics table to world map view

--- app/dash_app/views/demographic.py
+++ app/dash_app/views/demographic.py
@@ -20,7 +20,8 @@ def demographic_report(request):
         'dash_app/template.html',
         context={
             **get_tabs(request),
-            **dash_config
+            **dash_config,
+            "title": "Demographic metrics"
         }
     )
 
