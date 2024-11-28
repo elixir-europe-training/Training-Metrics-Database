85a6a50166d7c19ac8446824026ea797445cb87d
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2024-11-28 14:32:02 +0100

    fix: improve timeout management and user interacion

--- app/metrics/token.py
+++ app/metrics/token.py
@@ -12,20 +12,13 @@ def validate_token(token):
             public_key = key_file.read()
 
         token = base64.b64decode(token.encode("utf-8")).decode("utf-8")
-        data = jwt.decode(token, public_key, algorithms=["RS256"])
+        data = jwt.decode(token, public_key, options={"require": ["exp"]}, algorithms=["RS256"])
         username = data["username"]
         tick = data["tick"]
-        valid_to = datetime.datetime.fromisoformat(data["valid_to"])
-        last_valid_time = (
-            datetime.datetime.now() +
-            datetime.timedelta(seconds=settings.AUTH_TOKEN_MAX_LIFE_TIME)
-        )
         user = User.objects.get(username=username)
         user_tick = user.profile.auth_ticker
         if (
             user.profile.allow_token_reset and
-            valid_to <= last_valid_time and
-            valid_to > datetime.datetime.now() and
             tick == user_tick
         ):
             return user
@@ -35,20 +28,29 @@ def validate_token(token):
     raise ValidationError("Token is not valid")
 
 
+def get_content(token):
+    token = base64.b64decode(token.encode("utf-8")).decode("utf-8")
+    return jwt.decode(token, options={"verify_signature": False})
+
+
 def create_signup_token(user):
     valid_to = (
-        datetime.datetime.now() +
+        datetime.datetime.now(tz=datetime.timezone.utc) +
         datetime.timedelta(seconds=settings.AUTH_TOKEN_MAX_LIFE_TIME)
     )
     payload = {
         "username": user.username,
         "tick": user.profile.auth_ticker,
-        "valid_to": valid_to.isoformat(),
+        "exp": valid_to,
     }
 
     with open(settings.AUTH_TOKEN_PRIVATE_KEY, "r") as key_file:
         private_key = key_file.read()
 
-    token = jwt.encode(payload, private_key, algorithm="RS256")
+    token = jwt.encode(
+        payload,
+        private_key,
+        algorithm="RS256"
+    )
 
     return base64.b64encode(token.encode("utf-8")).decode("utf-8")
--- app/metrics/views/signup.py
+++ app/metrics/views/signup.py
@@ -1,4 +1,5 @@
 import datetime
+import calendar
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
 from django import forms
@@ -6,9 +7,10 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Field, Fieldset, Submit, Div
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
-from metrics.token import validate_token
+from metrics.token import validate_token, get_content
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib import messages
 
 
 class SignupForm(forms.Form):
@@ -44,10 +46,24 @@ class SignupForm(forms.Form):
 
 def jwt_signup(request):
     form = SignupForm(request.POST or None)
+    try:
+        token = request.GET["token"]
+        token_content = get_content(token)
+        expiration = token_content["exp"]
+        now_timestamp = calendar.timegm(
+            datetime.datetime.now(tz=datetime.timezone.utc).utctimetuple()
+        )
+
+        if expiration < now_timestamp:
+            messages.warning(request, "The token has expired")
+
+    except (ValidationError, KeyError):
+        messages.warning(request, "The token is invalid")
+
     if request.method == "POST" and form.is_valid():
         try:
             form_data = form.cleaned_data
-            user = validate_token(request.GET["token"])
+            user = validate_token(token)
             user.set_password(form_data["password"])
             user.profile.auth_ticker += 1
             user.save()
@@ -66,4 +82,3 @@ def jwt_signup(request):
             "form": form,
         }
     )
-    token = request.GET["token"]
